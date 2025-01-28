104b0d9ebbd4383a5fa7979917807bc82a24df98
DJARUUN <caargajac@gmail.com>
2025-01-28 21:23:23 +0100

    Add button component

---
+++ components/VButton.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const { type = "button" } = defineProps<{ type: "button" | "submit" | "reset" }>()
+</script>
+
+<template>
+	<button
+		class="px-3 py-1.5 rounded-md bg-zinc-950 text-zinc-50 hover:bg-zinc-900 active:scale-[97.5%] transition-all ease-out cursor-pointer"
+		:type="type"
+	>
+		<slot />
+	</button>
+</template>
