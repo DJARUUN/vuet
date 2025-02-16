761f35bccf5fb8650ac62f4886749cc7ca235fce
DJARUUN <caargajac@gmail.com>
2025-02-16 23:07:45 +0100

    chore(docs): Maintenance

--- vuet-docs/nuxt.config.ts
+++ vuet-docs/nuxt.config.ts
@@ -4,10 +4,24 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
+
   components: [{ path: "~/components", pathPrefix: false }],
+
   vite: {
     plugins: [tailwindcss()],
   },
+
   css: ["~/assets/css/main.css"],
+
   modules: ["@nuxt/fonts", "@nuxt/content"],
+
+  content: {
+    build: {
+      markdown: {
+        toc: {
+          depth: 3,
+        },
+      },
+    },
+  },
 });
