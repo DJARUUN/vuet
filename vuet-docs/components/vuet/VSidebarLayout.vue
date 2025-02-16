f70bb65ea4c26a4325d9adc1ae8f37482056551e
DJARUUN <caargajac@gmail.com>
2025-02-16 17:16:06 +0100

    feat(vuet): Add SidebarLayout component

---
+++ vuet-docs/components/vuet/VSidebarLayout.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="grid lg:grid-cols-[auto_1fr] not-lg:grid-rows-[auto_1fr] lg:gap-3 h-dvh overflow-hidden">
+    <slot name="sidebar" />
+
+    <div class="p-6 overflow-auto flex flex-col gap-6">
+      <slot />
+    </div>
+  </div>
+</template>
