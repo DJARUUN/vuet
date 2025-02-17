9fc335a62ec9d163c011fb6adbdfa71485bf4dca
DJARUUN <caargajac@gmail.com>
2025-02-17 13:47:28 +0100

    feat(docs): Make scrol position go to top of page when navigating

--- vuet-docs/pages/index.vue
+++ vuet-docs/pages/index.vue
@@ -1,3 +1,7 @@
 <script setup lang="ts">
 await navigateTo("/getting-started/introduction");
 </script>
+
+<template>
+	<div>Hello</div>
+</template>
---
+++ vuet-docs/plugins/fix-scroll-position.ts
@@ -0,0 +1,10 @@
+export default defineNuxtPlugin(() => {
+    useNuxtApp().hook("page:transition:finish", () => {
+        const main = document.querySelector("main");
+        if (!main) return;
+
+        main.style.scrollBehavior = "auto";
+        main?.scrollTo(0, 0);
+        main.style.scrollBehavior = "smooth";
+    });
+});
