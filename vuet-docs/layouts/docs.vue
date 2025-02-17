c73c10801e64e80916eabf047234e70035e1ff09
DJARUUN <caargajac@gmail.com>
2025-02-17 11:51:01 +0100

    feat(docs): Max content width to 70 chars

--- vuet-docs/layouts/docs.vue
+++ vuet-docs/layouts/docs.vue
@@ -18,7 +18,11 @@ const pages = await queryCollectionNavigation("content");
     <template #sidebar>
       <VSidebar :items="pages">
         <template #headerLeft>
-          <VButton to="/getting-started/introduction" variant="ghost" class="text-xl font-bold -ml-3.5">
+          <VButton
+            to="/getting-started/introduction"
+            variant="ghost"
+            class="text-xl font-bold -ml-3.5"
+          >
             Vuet
           </VButton>
         </template>
@@ -26,16 +30,40 @@ const pages = await queryCollectionNavigation("content");
         <template #headerRight>
           <!-- <SearchModal /> -->
 
-          <VButton variant="ghost" size="icon" @click="theme = theme === 'dark' ? 'light' : 'dark'">
-            <svg v-if="theme === 'dark'" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-              stroke-width="1.5" stroke="currentColor" class="size-4.5">
-              <path stroke-linecap="round" stroke-linejoin="round"
-                d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z" />
+          <VButton
+            variant="ghost"
+            size="icon"
+            @click="theme = theme === 'dark' ? 'light' : 'dark'"
+          >
+            <svg
+              v-if="theme === 'dark'"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="size-4.5"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z"
+              />
             </svg>
-            <svg v-else xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-              stroke="currentColor" class="size-4.5">
-              <path stroke-linecap="round" stroke-linejoin="round"
-                d="M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z" />
+            <svg
+              v-else
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="size-4.5"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z"
+              />
             </svg>
           </VButton>
         </template>
@@ -43,12 +71,14 @@ const pages = await queryCollectionNavigation("content");
     </template>
 
     <div class="lg:grid lg:grid-cols-[1fr_16rem] not-lg:flex not-lg:flex-col-reverse gap-9">
-      <div class="overflow-auto scroll-smooth flex flex-col gap-6 scroll-pt-6">
+      <div class="overflow-auto scroll-smooth flex flex-col gap-6 scroll-pt-6 w-[clamp(0px,100%,70ch)] mx-auto">
         <slot />
         <SiblingPageButtons />
       </div>
 
-      <TOC />
+      <div class="not-lg:w-[clamp(0px,100%,70ch)] not-lg:mx-auto">
+        <TOC />
+      </div>
     </div>
   </VSidebarLayout>
 </template>
--- vuet-docs/vuet/VSidebar.vue
+++ vuet-docs/vuet/VSidebar.vue
@@ -1,17 +1,16 @@
 <script setup lang="ts">
-import type { ContentNavigationItem } from '@nuxt/content';
+import type { ContentNavigationItem } from "@nuxt/content";
 
 const { items } = defineProps<{ items: ContentNavigationItem[] }>();
 
 const isMobileNavShown = ref(false);
 
 const route = useRoute();
-watch(route, () => isMobileNavShown.value = false);
+watch(route, () => (isMobileNavShown.value = false));
 </script>
 
 <template>
-  <aside
-    class="hidden lg:flex flex-col gap-5 p-6 border-r dark:border-zinc-500/35 border-zinc-400/30 min-w-[18rem] overflow-auto">
+  <aside class="hidden lg:flex flex-col gap-5 p-6 border-r dark:border-zinc-500/35 border-zinc-400/30 min-w-[18rem] overflow-auto">
     <div class="inline-flex justify-between items-center">
       <slot name="header">
         <div class="inline-flex gap-3">
@@ -24,16 +23,24 @@ watch(route, () => isMobileNavShown.value = false);
       </slot>
     </div>
 
-    <template v-for="item in items" :key="item">
-      <VSidebarGroup v-if="item.page === false" :item="item" />
-      <VSidebarItem v-else :item="item" />
+    <template
+      v-for="item in items"
+      :key="item"
+    >
+      <VSidebarGroup
+        v-if="item.page === false"
+        :item="item"
+      />
+      <VSidebarItem
+        v-else
+        :item="item"
+      />
     </template>
 
     <slot name="footer" />
   </aside>
 
-  <nav
-    class="hidden not-lg:flex flex-col gap-5 px-6 py-4 border-b dark:border-zinc-500/35 border-zinc-400/30 min-w-[18rem] overflow-auto">
+  <nav class="hidden not-lg:flex flex-col gap-5 px-6 py-4 border-b dark:border-zinc-500/35 border-zinc-400/30 min-w-[18rem] overflow-auto">
     <div class="grid grid-cols-[1fr_auto] gap-3">
       <div class="inline-flex justify-between items-center">
         <slot name="header">
@@ -47,26 +54,63 @@ watch(route, () => isMobileNavShown.value = false);
         </slot>
       </div>
 
-      <VButton variant="ghost" size="icon" @click="isMobileNavShown = !isMobileNavShown">
-        <svg v-if="!isMobileNavShown" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-          stroke-width="1.5" stroke="currentColor" class="size-4.5">
-          <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+      <VButton
+        variant="ghost"
+        size="icon"
+        @click="isMobileNavShown = !isMobileNavShown"
+      >
+        <svg
+          v-if="!isMobileNavShown"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="size-4.5"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+          />
         </svg>
-        <svg v-else xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-          stroke="currentColor" class="size-4.5">
-          <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+        <svg
+          v-else
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="size-4.5"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M6 18 18 6M6 6l12 12"
+          />
         </svg>
       </VButton>
     </div>
   </nav>
 
   <Transition>
-    <div v-show="isMobileNavShown"
-      class="lg:hidden block fixed z-[100] top-16 dark:bg-zinc-900 bg-zinc-100 p-6 bottom-0 inset-x-0 pt-0 overflow-auto">
+    <div
+      v-show="isMobileNavShown"
+      class="lg:hidden block fixed z-[100] top-16 dark:bg-zinc-900 bg-zinc-100 p-6 bottom-0 inset-x-0 pt-0 overflow-auto"
+    >
       <div class="flex flex-col gap-5 transition-[scale] origin-top ease-bounce duration-300">
-        <template v-for="item in items" :key="item">
-          <VSidebarGroup v-if="item.page === false" :item="item" />
-          <VSidebarItem v-else :item="item" />
+        <template
+          v-for="item in items"
+          :key="item"
+        >
+          <VSidebarGroup
+            v-if="item.page === false"
+            :item="item"
+          />
+          <VSidebarItem
+            v-else
+            :item="item"
+          />
         </template>
       </div>
 
