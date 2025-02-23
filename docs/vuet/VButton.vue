fb831157474adbe23141796079a276b289f9c3b4
DJARUUN <caargajac@gmail.com>
2025-02-23 19:20:29 +0100

    fix(vuet): Make buttons work again

--- docs/vuet/VButton.vue
+++ docs/vuet/VButton.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-import { NuxtLink } from "#components";
 import { twMerge } from "tailwind-merge";
 import { RouterLink } from "vue-router";
 
@@ -8,7 +7,6 @@ const {
 	variant = "default",
 	size = "default",
 	linkType = "nuxt",
-	to
 } = defineProps<{
 	type?: "button" | "submit" | "reset";
 	variant?: keyof typeof variantStyles;
@@ -91,71 +89,67 @@ const innerStyles = "inline-flex gap-2 items-center justify-center size-full";
 </script>
 
 <template>
-	<template v-if="!to">
-		<button v-if="!to" :type="type" :disabled="loading || disabled || undefined"
-			:class="twMerge(sharedStyles, sizeStyle, variantStyles[variant], $props.class)">
-			<div :class="twMerge(innerStyles, innerClass)">
-				<slot v-if="loading" name="loading">
-					<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-						stroke="currentColor" class="size-4 animate-spin">
-						<path stroke-linecap="round" stroke-linejoin="round"
-							d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99" />
-					</svg>
-					<span v-if="loadingText">{{ loadingText }}</span>
-				</slot>
+	<button v-if="!to" :type="type" :disabled="loading || disabled || undefined"
+		:class="twMerge(sharedStyles, sizeStyle, variantStyles[variant], $props.class)">
+		<div :class="twMerge(innerStyles, innerClass)">
+			<slot v-if="loading" name="loading">
+				<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+					class="size-4 animate-spin">
+					<path stroke-linecap="round" stroke-linejoin="round"
+						d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99" />
+				</svg>
+				<span v-if="loadingText">{{ loadingText }}</span>
+			</slot>
 
-				<slot v-else />
-			</div>
-		</button>
-	</template>
+			<slot v-else />
+		</div>
+	</button>
 
-	<template v-else>
-		<NuxtLink v-if="linkType === 'nuxt'" :disabled="loading || disabled || undefined"
-			:class="twMerge(sharedStyles, sizeStyle, variantStyles[variant], $props.class)" :to="to">
-			<div :class="twMerge(innerStyles, innerClass)">
-				<slot v-if="loading" name="loading">
-					<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-						stroke="currentColor" class="size-4 animate-spin">
-						<path stroke-linecap="round" stroke-linejoin="round"
-							d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99" />
-					</svg>
-					<span v-if="loadingText">{{ loadingText }}</span>
-				</slot>
+	<NuxtLink v-else-if="linkType === 'nuxt'" :disabled="loading || disabled || undefined"
+		:class="twMerge(sharedStyles, sizeStyle, variantStyles[variant], $props.class)" :to="to">
+		<div :class="twMerge(innerStyles, innerClass)">
+			<slot v-if="loading" name="loading">
+				<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+					class="size-4 animate-spin">
+					<path stroke-linecap="round" stroke-linejoin="round"
+						d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99" />
+				</svg>
+				<span v-if="loadingText">{{ loadingText }}</span>
+			</slot>
 
-				<slot v-else />
-			</div>
-		</NuxtLink>
+			<slot v-else />
+		</div>
+	</NuxtLink>
 
-		<RouterLink v-else-if="linkType === 'vue'" :disabled="loading || disabled || undefined"
-			:class="twMerge(sharedStyles, sizeStyle, variantStyles[variant], $props.class)" :to="to">
-			<div :class="twMerge(innerStyles, innerClass)">
-				<slot v-if="loading" name="loading">
-					<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-						stroke="currentColor" class="size-4 animate-spin">
-						<path stroke-linecap="round" stroke-linejoin="round"
-							d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99" />
-					</svg>
-					<span v-if="loadingText">{{ loadingText }}</span>
-				</slot>
+	<RouterLink v-else-if="linkType === 'vue'" :disabled="loading || disabled || undefined"
+		:class="twMerge(sharedStyles, sizeStyle, variantStyles[variant], $props.class)" :to="to">
+		<div :class="twMerge(innerStyles, innerClass)">
+			<slot v-if="loading" name="loading">
+				<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+					class="size-4 animate-spin">
+					<path stroke-linecap="round" stroke-linejoin="round"
+						d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99" />
+				</svg>
+				<span v-if="loadingText">{{ loadingText }}</span>
+			</slot>
 
-				<slot v-else />
-			</div>
-		</RouterLink>
+			<slot v-else />
+		</div>
+	</RouterLink>
 
-		<a v-else :disabled="loading || disabled || undefined"
-			:class="twMerge(sharedStyles, sizeStyle, variantStyles[variant], $props.class)" :href="to">
-			<div :class="twMerge(innerStyles, innerClass)">
-				<slot v-if="loading" name="loading">
-					<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-						stroke="currentColor" class="size-4 animate-spin">
-						<path stroke-linecap="round" stroke-linejoin="round"
-							d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99" />
-					</svg>
-					<span v-if="loadingText">{{ loadingText }}</span>
-				</slot>
+	<a v-else :disabled="loading || disabled || undefined"
+		:class="twMerge(sharedStyles, sizeStyle, variantStyles[variant], $props.class)" :href="to">
+		<div :class="twMerge(innerStyles, innerClass)">
+			<slot v-if="loading" name="loading">
+				<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+					class="size-4 animate-spin">
+					<path stroke-linecap="round" stroke-linejoin="round"
+						d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99" />
+				</svg>
+				<span v-if="loadingText">{{ loadingText }}</span>
+			</slot>
 
-				<slot v-else />
-			</div>
-		</a>
-	</template>
+			<slot v-else />
+		</div>
+	</a>
 </template>
