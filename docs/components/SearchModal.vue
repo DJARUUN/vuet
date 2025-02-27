d2436685854774c340841936660fe659f27835ef
DJARUUN <caargajac@gmail.com>
2025-02-27 18:47:53 +0100

    tweak(docs): Tweak points to remove text parts in search button to be more exact

--- docs/components/SearchModal.vue
+++ docs/components/SearchModal.vue
@@ -60,9 +60,9 @@ onBeforeUnmount(() => window.removeEventListener("keydown", handleKeyDown));
     <VButton variant="outline" @click="isSearchModalOpen = true"
       :class="twMerge(`@container text-muted-fg relative justify-start`, buttonClass)">
       <MagnifyingGlassIcon
-        class="size-4.5 shrink-0 @max-[5.5rem]:absolute @max-[5.5rem]:left-[50%] @max-[5.5rem]:-translate-x-[50%]" />
+        class="size-4.5 shrink-0 @max-[5.1rem]:absolute @max-[5.1rem]:left-[50%] @max-[5.1rem]:-translate-x-[50%]" />
 
-      <span class="@max-[5.5rem]:hidden">Search<span class="@max-[7.5rem]:hidden"> docs</span>...</span>
+      <span class="@max-[5.1rem]:hidden">Search<span class="@max-[7.4rem]:hidden"> docs</span>...</span>
 
       <VKbd noPlus class="not-md:hidden absolute right-1.5">
         <span>⌘</span><span>K</span>
