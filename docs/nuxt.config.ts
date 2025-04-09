6dd48a1f2e5e20bc4b7c1e6c69d764ce5bd1f369
DJARUUN <caargajac@gmail.com>
2025-04-09 19:51:18 +0200

    fix(docs): Maybe fix prerender? or not?

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -2,6 +2,7 @@ import tailwindcss from "@tailwindcss/vite";
 import { resolve } from "path";
 import { fileURLToPath } from "url";
 import { globby } from "globby";
+import { prerenderRoutes } from "#app";
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
@@ -53,11 +54,11 @@ export default defineNuxtConfig({
   },
 
   hooks: {
-    "nitro:build:before": async ({
+    async "nitro:build:before"({
       options: {
-        prerender: { routes },
+        prerender: { routes: nuxtRoutes },
       },
-    }) => {
+    }) {
       console.log("Finding content pages to prerender");
 
       const __dirname = fileURLToPath(new URL(".", import.meta.url));
@@ -80,7 +81,7 @@ export default defineNuxtConfig({
 
         console.log(`Found ${routes.length} content routes to prerender`);
 
-        routes.forEach((route) => routes.push(route));
+        routes.forEach((route) => nuxtRoutes.push(route));
       } catch (error) {
         console.error("Error finding content files:", error);
       }
@@ -90,4 +91,39 @@ export default defineNuxtConfig({
   ogImage: {
     fonts: ["Bricolage+Grotesque:700", "Geist:400"],
   },
+
+  // sitemap: {
+  //   async urls() {
+  //     console.log("Finding content pages to prerender");
+
+  //     const __dirname = fileURLToPath(new URL(".", import.meta.url));
+  //     const contentDir = resolve(__dirname, "content");
+
+  //     try {
+  //       const contentFiles = await globby(
+  //         ["**/*.md", "**/*.yaml", "**/*.json"],
+  //         {
+  //           cwd: contentDir,
+  //         }
+  //       );
+
+  //       const routes = contentFiles.map((file) => {
+  //         const path =
+  //           "/" +
+  //           file.replace(/\.(md|yaml|json)$/, "").replaceAll(/([0-9])\./g, "");
+  //         return path.endsWith("/index") ? path.replace(/\/index$/, "/") : path;
+  //       });
+
+  //       console.log(`Found ${routes.length} content routes to prerender`);
+
+  //       routes.forEach((route) => routes.push(route));
+
+  //       return routes;
+  //     } catch (error) {
+  //       console.error("Error finding content files:", error);
+
+  //       return [];
+  //     }
+  //   },
+  // },
 });
